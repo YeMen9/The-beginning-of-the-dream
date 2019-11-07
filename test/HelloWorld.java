05e8be2bdecc96e7dca70832e0dbb3859392b139
YeMen9 <1259558773@qq.com>
2019-11-07 18:01:13 +0800

    Signed-off-by: YeMen9 <1259558773@qq.com>

---
+++ test/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author swx
+ * @company
+ * @create 2019-11-07 17:57
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
